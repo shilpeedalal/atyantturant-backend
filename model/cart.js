290ae4ad08e4f56cc9386ed20ebfa9d0a3b77a93
Shilpee <shilpeedalal937@gmail.com>
2023-09-29 18:57:30 +0530

    changes in value

--- model/cart.js
+++ model/cart.js
@@ -12,7 +12,7 @@ const cartSchema = new Schema({
     quantity:{
         type:Number,
         required:true,
-        default:1
+        default:0
     },
     total_amount:{
         type: Number,
